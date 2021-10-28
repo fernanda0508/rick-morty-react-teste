78616a2d3a7e11228410ed19daebb4de1aaf63bb
fernanda0508 <fefecoelho0508@gmail.com>
2021-10-28 20:20:55 -0300

    Criando o header responsivo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World
+      <header className="header">
+        Hello header
+
+      </header>
     </div>
   );
 }
